65b054c5ae8742b8f58276f5073960dd2a6cbd2c
Berk Erdem <berk.erdem@rwth-aachen.de>
2026-07-05 21:18:08 +0200

    [refactoring]: access

--- src/drivers/blk/mod.rs
+++ src/drivers/blk/mod.rs
@@ -200,13 +200,13 @@ impl VirtioBlkDriver {
 		// 	}
 		// }
 
-		match self.test_seq_read_sdmmc() {
-			Ok(()) => info!("Sequential read test successful!"),
-			Err(e) => {
-				error!("Sequential read test: {e:?}");
-				return Err(VirtioBlkError::BlkDevError(self.get_dev_id()));
-			}
-		}
+		// match self.test_seq_read_sdmmc() {
+		// 	Ok(()) => info!("Sequential read test successful!"),
+		// 	Err(e) => {
+		// 		error!("Sequential read test: {e:?}");
+		// 		return Err(VirtioBlkError::BlkDevError(self.get_dev_id()));
+		// 	}
+		// }
 
 		// match self.test_rnd_write_sdmmc() {
 		// 	Ok(()) => info!("Random sdmmc write test successful!"),
